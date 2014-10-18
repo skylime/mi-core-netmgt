3eb26891f522c72a64f3b95395ec4a9225d1d7cb
Thomas Merkel <tm@core.io>
2014-10-18 14:26:40 +0200

    Create delegate dataset for /var/db database files

---
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+UUID=$(mdata-get sdc:uuid)
+DDS=zones/${UUID}/data
+
+if zfs list ${DDS} 1>/dev/null 2>&1; then
+	zfs create ${DDS}/db || true
+
+	zfs set mountpoint=/var/db ${DDS}/db
+fi

a84b48174a9d8ef47ddf936e27adab955d1ce3e0
Thomas Merkel <tm@core.io>
2014-10-18 15:29:49 +0200

    Add script to configure netmgt basic setup

---
+++ copy/var/zoneinit/includes/50-netmgt.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Setup netmgt configuration
+
+ALLOWED_HOST=$(hostname)
+SECRET_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-100})
+
+NETMGT_ADMIN=${ADMIN_NETMGT:-$(mdata-get netmgt_admin 2>/dev/null)} || \
+NETMGT_ADMIN=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
+mdata-put netmgt_admin ${NETMGT_ADMIN}
+
+NETMGT_DEFAULT_NAMESERVERS=$(
+	echo 'NETMGT_DEFAULT_NAMESERVERS = [';
+	for nameserver in $(mdata-get netmgt_default_nameservers 1>/dev/null 2>&1); do
+		echo "'${nameserver}', "
+	done
+	echo ']'
+)
+
+if mdata-get netmgt_hostmaster 1>/dev/null 2>&1; then
+	NETMGT_HOSTMASTER='NETMGT_HOSTMASTER = "'$(mdata-get netmgt_hostmaster)'"'
+else
+	NETMGT_HOSTMASTER='NETMGT_HOSTMASTER = "hostmaster@'${ALLOWED_HOST}'"'
+fi
+
+NETMGT_DNS_TOKEN=${NETMGT_DNS_TOKEN:-$(mdata-get netmgt_dns_token 2>/dev/null)} || \
+NETMGT_DNS_TOKEN=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
+mdata-put netmgt_dns_token ${NETMGT_DNS_TOKEN}
+
+cat >> /opt/netmgt/netmgt_web/settings.py <<EOF
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = "${SECRET_KEY}"
+
+# Allow only the hostname and localhost to access
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '${ALLOWED_HOST}']
+
+# NETMGT
+NETMGT_DEFAULT_TTL = 3600
+NETMGT_SOA = {
+	'refresh': '2d',
+	'retry':   '15M',
+	'expiry':  '2w',
+	'minimum': '1h',
+}
+${NETMGT_DEFAULT_NAMESERVERS}
+${NETMGT_HOSTMASTER}
+NETMGT_DNS_TOKEN = '${NETMGT_DNS_TOKEN}'
+EOF
+
+# Init django data and create admin user
+/opt/netmgt/manage.py syncdb --noinput
+
+# Create superadmin user
+if [[ ! -e '/var/db/netmgt.sqlite3' ]]; then
+	echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '${netmgt_admin}')" \
+		| /opt/netmgt/manage.py shell
+fi

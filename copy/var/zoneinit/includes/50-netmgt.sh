6ae1860ef33b2033a87786543864b10687960dde
Thomas Merkel <tm@core.io>
2019-10-29 10:32:06 +0100

    use tooling from core-base to setup netmgt

--- copy/var/zoneinit/includes/50-netmgt.sh
+++ copy/var/zoneinit/includes/50-netmgt.sh
@@ -4,10 +4,8 @@
 ALLOWED_HOST=$(hostname)
 SECRET_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-100})
 
-NETMGT_ADMIN=${ADMIN_NETMGT:-$(mdata-get netmgt_admin 2>/dev/null)} || \
-NETMGT_ADMIN=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
-mdata-put netmgt_admin ${NETMGT_ADMIN}
-
+NETMGT_ADMIN_INITIAL_PW=$(/opt/core/bin/mdata-create-password.sh -m netmgt_admin_initial_pw)
+NETMGT_DNS_TOKEN=$(/opt/core/bin/mdata-create-password.sh -m netmgt_dns_token)
 NETMGT_DEFAULT_NAMESERVERS=$(
 	echo 'NETMGT_DEFAULT_NAMESERVERS = [';
 	for nameserver in $(mdata-get netmgt_default_nameservers 2>/dev/null); do
@@ -16,20 +14,14 @@ NETMGT_DEFAULT_NAMESERVERS=$(
 	echo ']'
 )
 
+NETMGT_HOSTMASTER='NETMGT_HOSTMASTER = "hostmaster@'${ALLOWED_HOST}'"'
 if mdata-get netmgt_hostmaster 1>/dev/null 2>&1; then
 	NETMGT_HOSTMASTER='NETMGT_HOSTMASTER = "'$(mdata-get netmgt_hostmaster)'"'
-else
-	NETMGT_HOSTMASTER='NETMGT_HOSTMASTER = "hostmaster@'${ALLOWED_HOST}'"'
 fi
-
-NETMGT_DNS_TOKEN=${NETMGT_DNS_TOKEN:-$(mdata-get netmgt_dns_token 2>/dev/null)} || \
-NETMGT_DNS_TOKEN=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
-mdata-put netmgt_dns_token ${NETMGT_DNS_TOKEN}
 
 NETMGT_EXPORT_PREFIX=${NETMGT_EXPORT_PREFIX:-$(mdata-get netmgt_export_prefix 2>/dev/null)} || \
 NETMGT_EXPORT_PREFIX=$(mdata-get sdc:uuid);
 mdata-put netmgt_export_prefix ${NETMGT_EXPORT_PREFIX}
-
 
 cat >> /opt/netmgt/netmgt_web/settings.py <<EOF
 # Static files location
@@ -56,11 +48,15 @@ NETMGT_EXPORT_PREFIX = '${NETMGT_EXPORT_PREFIX}'
 EOF
 
 # Init django data and create admin user
-/opt/netmgt/manage.py migrate --noinput
+/opt/netmgt/manage.py migrate --noinput --fake-initial
 
 # Create superadmin user
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '${NETMGT_ADMIN}')" \
-	| /opt/netmgt/manage.py shell || true
+cat <<eof | /opt/netmgt/manage.py shell
+from django.contrib.auth import get_user_model
+user = get_user_model()
+user.objects.filter(username="admin).exists() or \
+    user.objects.create_superuser("admin", "", "${NETMGT_ADMIN_INITIAL_PW}")
+eof
 
 # Copy all static files to /opt/netmgt/static
 /opt/netmgt/manage.py collectstatic --noinput

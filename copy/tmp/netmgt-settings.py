a50d3dd5018d346db9a2a510a11555a96702a101
Thomas Merkel <tm@core.io>
2019-10-29 10:31:47 +0100

    switch to new netmgt settings for latest django version

--- copy/tmp/netmgt-settings.py
+++ copy/tmp/netmgt-settings.py
@@ -1,10 +1,8 @@
 import os
-from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DEBUG = False
-TEMPLATE_DEBUG = False
 
 INSTALLED_APPS = (
 	'django.contrib.admin',
@@ -13,28 +11,19 @@ INSTALLED_APPS = (
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+	'rest_framework',
 	'netmgt',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = TCP + (
-	'django.core.context_processors.request',
-)
-
-SUIT_CONFIG = {
-	'ADMIN_NAME': 'Network Management Tool',
-	'SEARCH_URL': '',
-}
-
+]
 
 ROOT_URLCONF = 'netmgt_web.urls'
 WSGI_APPLICATION = 'netmgt_web.wsgi.application'
@@ -53,6 +42,21 @@ CACHES = {
 	}
 }
 
+TEMPLATES = [
+{
+	'BACKEND': 'django.template.backends.django.DjangoTemplates',
+	'DIRS': [os.path.join(BASE_DIR,'templates')],
+	'APP_DIRS': True,
+	'OPTIONS': {
+		'context_processors': [
+			'django.template.context_processors.debug',
+			'django.template.context_processors.request',
+			'django.contrib.auth.context_processors.auth',
+			'django.contrib.messages.context_processors.messages',
+		],
+	},
+},]
+
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
@@ -61,6 +65,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (
-	BASE_DIR + '/templates/',
-)
+
+# DRF
+REST_FRAMEWORK = {
+	'DEFAULT_RENDERER_CLASSES': [
+		'rest_framework.renderers.JSONRenderer',
+		'rest_framework.renderers.BrowsableAPIRenderer',
+	]
+}
